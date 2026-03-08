af70c177c305d6260258c9375376aec10ac596ae
devifyo <sunilthagyal355@gmail.com>
2026-03-08 19:44:25 +0000

    added fav icon

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Admin Portal') - {{ config('app.name') }}</title>
-
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
         tailwind.config = {
